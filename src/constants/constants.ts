e72888e74369e4406f89bfa0f6989dbb0c5849d0
DeiNas77 <deinas.44remastered@gmail.com>
2026-06-17 21:02:03 -0500

    STK-84 | chore: change nameLink'inicio' for 'Inicio'

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 import { routes } from "./routes.ts";
 
 export const linkNavegation = [
-  { link: routes.Home, name: "inicio" },
+  { link: routes.Home, name: "Inicio" },
   { link: routes.Accommodation, name: "Alojamientos" },
   { link: routes.Experiences, name: "Experiencias" },
   { link: routes.AboutUs, name: "Sobre Nosotros" },
