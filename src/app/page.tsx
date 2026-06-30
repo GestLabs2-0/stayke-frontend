b9a12a7880ccfa8d7ad0062b8a088e8226439ed4
DeiNas77 <deinas.44remastered@gmail.com>
2026-06-30 17:51:07 -0500

    STK-86 | WIP: add filterBar for Desktop Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { FilterBar } from "@/components/home/FilterBar/FilterBar";
 import { Header } from "@/components/home/Header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <section className="pb-6 pt-10">
+        <FilterBar />
+      </section>
     </div>
   );
 }
