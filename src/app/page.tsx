a6c6531f0fc32e0885552fb184eb7dca050d093c
gabotdev <gabrieltorres2014@gmail.com>
2026-07-03 09:02:01 -0400

    STK-87 | fix: fix biome check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { EscapadasCerca } from "@/components/home/EscapadasCerca";
+import { propertiesData } from "@/components/home/EscapadasCerca/mocks";
 import { Header } from "@/components/home/Header";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Header />
+      <EscapadasCerca
+        properties={propertiesData}
+        periodLabel="12 - 14 de junio"
+      />
     </div>
   );
 }
