59e4997bc3d8cfff302a649a7b870021a6addf80
darkkevo <kevinaraujogonzalez@gmail.com>
2026-07-12 18:31:29 -0400

    fix: arreglados algunas observaciones
    fix: arreglado algunos detalles, agregado el slider, y responsive

--- src/components/home/Testimonios/Testimonios.tsx
+++ src/components/home/Testimonios/Testimonios.tsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { BlossomCarousel } from "@blossom-carousel/react";
+import { SectionWrapper } from "@/components/shared/SectionWrapper";
 import { testimoniosData } from "./mocks";
 import { TestimonioCard } from "./TestimonioCard";
 
 export const Testimonios = () => {
   return (
-    <section className="w-full py-16 px-6 md:px-10 lg:px-12">
-      <div className="max-w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
+    <SectionWrapper>
+      {/* Desktop: grid */}
+      <div className="hidden min-[700px]:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
         {testimoniosData.map((testimonio) => (
           <TestimonioCard key={testimonio.id} testimonio={testimonio} />
         ))}
       </div>
-    </section>
+
+      {/* Mobile: carousel */}
+      <div className="min-[700px]:hidden">
+        <BlossomCarousel load="conditional">
+          {testimoniosData.map((testimonio) => (
+            <span key={testimonio.id} className="inline-block mx-2 first:ml-0">
+              <TestimonioCard testimonio={testimonio} />
+            </span>
+          ))}
+        </BlossomCarousel>
+      </div>
+    </SectionWrapper>
   );
 };
